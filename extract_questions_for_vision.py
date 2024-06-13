224329d9be1ebc22f86aa1d6e262178b77be8080
Jayanth-DS-Intern <Jayanth.K@fixitylearn.com>
2024-06-13 17:00:00 +0530

    Text model with teacher solutions also added

--- extract_questions_for_vision.py
+++ extract_questions_for_vision.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 import json
 import os,time
 import sys
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 load_dotenv()
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
